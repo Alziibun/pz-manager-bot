8c55711d7f6ce71aa2026ca2f5b83cc8766b8f23
Alice S <snapp@null.net>
2023-07-26 18:04:34 -0400

    Move discord code to a separate class

--- src/main/java/com/github/alziibun/Main.java
+++ src/main/java/com/github/alziibun/Main.java
@@ -10,36 +10,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // init config file
-        File configFile = new File("bot.config");
-        Properties config = new Properties();
-        try {
-            FileReader reader = new FileReader(configFile);
-            config.load(reader);
-        } catch (FileNotFoundException ex) {
-            System.out.println("Uh oh");
-        } catch (IOException ex) {
-            System.out.println("why");
-        }
-
-        if (config.getProperty("token") == null) {
-            try {
-                Scanner input = new Scanner(System.in);
-                FileWriter writer = new FileWriter(configFile);
-                System.out.print("Discord Bot Token: ");
-                String token = input.nextLine();
-                config.setProperty("token", token);
-                config.store(writer, "token");
-                writer.close();
-            } catch (IOException ex) {
-                System.out.println("lol");
-            }
-        }
-        DiscordApi api = new DiscordApiBuilder()
-                .setToken(config.getProperty("token"))
-                .addServerBecomesAvailableListener( event -> {
-                    System.out.println("Loaded " + event.getServer().getName());
-                })
-                .login()
-                .join();
+        Discord.init();
     }
 }
